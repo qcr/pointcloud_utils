c965eb8aefaef15c25e60989e79528934f4bb49a
Das <dasun.gunasinghe@qut.edu.au>
2023-08-10 17:47:42 +1000

    removal of methods not required

--- src/pointcloud_utils/__init__.py
+++ src/pointcloud_utils/__init__.py
@@ -5,19 +5,9 @@ import cv_bridge
 import tf2_ros
 import rospy
 from geometry_msgs.msg import Pose, TransformStamped
-from sensor_msgs.msg import Image, CameraInfo, PointField, PointCloud2
-import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import Image, CameraInfo
 
 bridge = cv_bridge.CvBridge()
-
-# The data structure of each point in ros PointCloud2: 16 bits = x + y + z + rgb
-FIELDS_XYZ = [
-    PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
-    PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
-    PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
-]
-FIELDS_XYZRGB = FIELDS_XYZ + \
-    [PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)]
 
 def ros_to_rgbd(color_msg: Image, depth_msg: Image, corner1=None, corner2=None) -> o3d.geometry.RGBDImage:
     depth_img = bridge.imgmsg_to_cv2(depth_msg, desired_encoding='passthrough')
@@ -77,44 +67,6 @@ def xyz_to_uv(pcd: o3d.geometry.PointCloud, camera_model: o3d.camera.PinholeCame
     v = points[:,1] * fy / points[:,2] + cy
 
     return np.c_[u.astype(np.int32), v.astype(np.int32)]
-
-# This is taken from https://github.com/felixchenfy/open3d_ros_pointcloud_conversion/blob/master/lib_cloud_conversion_between_Open3D_and_ROS.py
-def pointcloud2_to_pcd(pointcloud2: PointCloud2):
-    
-    # Get cloud data from ros_cloud
-    field_names=[field.name for field in pointcloud2.fields]
-    cloud_data = list(pc2.read_points(pointcloud2, skip_nans=True, field_names = field_names))
-
-    # Check empty
-    open3d_cloud = o3d.PointCloud()
-    if len(cloud_data)==0:
-        print("Converting an empty cloud")
-        return None
-
-    # Set open3d_cloud
-    if "rgb" in field_names:
-        IDX_RGB_IN_FIELD=3 # x, y, z, rgb
-        
-        # Get xyz
-        xyz = [(x,y,z) for x,y,z,rgb in cloud_data ] # (why cannot put this line below rgb?)
-
-        # Get rgb
-        # Check whether int or float
-        if type(cloud_data[0][IDX_RGB_IN_FIELD])==float: # if float (from pcl::toROSMsg)
-            rgb = [convert_rgbFloat_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
-        else:
-            rgb = [convert_rgbUint32_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
-
-        # combine
-        open3d_cloud.points = open3d.Vector3dVector(np.array(xyz))
-        open3d_cloud.colors = open3d.Vector3dVector(np.array(rgb)/255.0)
-    else:
-        xyz = [(x,y,z) for x,y,z in cloud_data ] # get xyz
-        open3d_cloud.points = open3d.Vector3dVector(np.array(xyz))
-
-    # return
-    return open3d_cloud
-
 
 def reproject(pcd: o3d.geometry.PointCloud, camera_info: CameraInfo, img: np.ndarray = None):
   indices, _ = cv2.projectPoints(
